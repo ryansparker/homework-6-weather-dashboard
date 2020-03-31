cf5209fa968541de31fbb4f2fe195a7952ab6798
Ryan Parker <ryansparker@gmail.com>
2020-03-30 23:08:16 -0400

    got the js to cnnect to the divs in the index.html display

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+
+
 var button =document.querySelector(".button");
 var inputValue = document.querySelector(".inputValue")
 var place = document.getElementById("place");
@@ -7,11 +9,11 @@ var windspeed = document.getElementById("windy");
 var UVp = document.getElementById("UV");
 
 
-
 button.addEventListener("click", function(){
-    fetch("https://api.openweathermap.org/data/2.5/weather?q="+inputValue.value+"&appid=974d902c878dcae370e669f524ad6ba0")
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="+inputValue.value+"&appid=974d902c878dcae370e669f524ad6ba0&units=imperial")
 .then (response => response.json())
 .then (data => {
+
     var placeValue = data["name"];
     var degreesValue = data["main"]["temp"];
     var humValue = data["main"]["humidity"];
@@ -19,11 +21,34 @@ button.addEventListener("click", function(){
     var UVValue = data["main"]["humidity"];
 
     place.innerHTML = placeValue;
-    degrees.innerHTML = degreesValue;
-    humy.innerHTML = humValue;
-    windspeed.innerHTML = windValue;
-    UVp.innerHTML = UVValue;
+    degrees.innerHTML = "Temperature: " + degreesValue + "° F";
+    humy.innerHTML = "Humidity: " + humValue + "%";
+    windspeed.innerHTML = "Wind Speed: " + windValue + " kpr";
+    UVp.innerHTML = "UV: " + UVValue;
 
 })
 
 });
+
+
+//----------------------------------------------------------------
+//experimenting with Bujumbura code
+
+// var inputValue = document.querySelector(".inputValue")
+// var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+inputValue.value+"&appid=974d902c878dcae370e669f524ad6ba0"
+
+//     // Here we run our AJAX call to the OpenWeatherMap API
+
+// button.addEventListener("click", function(){
+
+//     $.ajax ({
+//     url: queryURL,
+//     method: "GET"
+// })
+
+// .then(function(response){
+
+//     $("#city").html("<h1>" + response.name + "</h1>");
+//     $("#wind").text("Wind Speed: " + response.wind.speed);
+// })
+// });
