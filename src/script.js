c1a88f6f8b0601316b63d9bbe563f098e2af8cfb
Ryan Parker <ryansparker@gmail.com>
2020-04-02 18:38:52 -0400

    the UV Api is working

--- src/script.js
+++ src/script.js
@@ -1,79 +1,127 @@
 
-
+// VARIABLES SEARCH
 var button = document.querySelector(".button");
 var inputValue = document.querySelector(".inputValue")
 
-var place = document.getElementById("place");
+// VARIABLES PAST SEARCH
+var searched = document.getElementById("searchedCity");
+var historyArray = [];
 
+// VARIABLES DISPLAY
+var place = document.getElementById("place");
 var degrees = document.getElementById("degrees");
 var humy = document.getElementById("hum");
 var windspeed = document.getElementById("windy");
-var UVp = document.getElementById("UV");
+var UVIndex = document.getElementById("UV");
 
-var searched = document.getElementById("searchedCity");
+//VARIABLES 5 DAY DISPLAY
+var dateFivep = document.getElementsByClassName("dateFive");
+var weatherFivep = document.getElementsByClassName("weatherFive");
+var tempFivep = document.getElementsByClassName("tempFive");
+var humFivep = document.getElementsByClassName("humFive");
 
-var historyArray = [];
 
 // button.addEventListener("click", function(){
 $(document).ready(function () {
     $("#subBtn").on("click", function () {
-    var city = inputValue.value;
-    searchWeather(city);    
-    searchFiveDay(city);        
+        var city = inputValue.value;
+        searchWeather(city);
+        searchFiveDay(city);
 
     });
-    $("#searchedCity").on("click", "h5", function(){
+    $("#searchedCity").on("click", "h5", function () {
         searchWeather($(this).text());
         searchFiveDay(city);
     })
 
     //SEARCH DAILY WEATHER FUNCTION
 
-   function searchWeather(city) {
-       fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=974d902c878dcae370e669f524ad6ba0&units=imperial")
+    function searchWeather(city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=974d902c878dcae370e669f524ad6ba0&units=imperial")
+
             .then(response => response.json())
             .then(data => {
 
                 var placeValue = data["name"];
 
                 var degreesValue = data["main"]["temp"];
-                var humValue = data["main"]["humidity"];
+
                 var windValue = data["wind"]["speed"];
-                // var UVValue = data["main"]["humidity"];
+
+                var humValue = data["main"]["humidity"];
 
                 place.innerHTML = placeValue + " ( " + new Date().toLocaleDateString() + " )";
                 degrees.innerHTML = "Temperature: " + degreesValue + "° F";
+                windspeed.innerHTML = "Wind Speed: " + windValue + " kph";
                 humy.innerHTML = "Humidity: " + humValue + "%";
-                windspeed.innerHTML = "Wind Speed: " + windValue + " kpr";
 
-                if(historyArray.indexOf(placeValue)===-1){
+
+
+                //UV INDEX
+
+                var x = data["coord"]["lat"];
+                var y = data["coord"]["lon"];
+
+                fetch("http://api.openweathermap.org/data/2.5/uvi?appid=974d902c878dcae370e669f524ad6ba0&lat=" + x + "&lon=" + y + "")
+                .then(response => response.json())
+                .then(data => {
+
+
+                var UVValue = data["value"];
+
+                UVIndex.innerHTML = "UV Index: " + UVValue;
+                })
+
+
+                //PREVIOUS SEARCH LIST
+                if (historyArray.indexOf(placeValue) === -1) {
                     historyArray.push(placeValue);
                     var button = $("<h5>").text(placeValue);
                     $("#searchedCity").prepend(button);
                 }
-                // UVp.innerHTML = "UV: " + UVValue;
-            
-                
+
+
+
             })
-   }
 
-   //5 DAY FORECAST FUNCTION
+    }
 
-   function searchFiveDay(city) {
-       fetch ("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=974d902c878dcae370e669f524ad6ba0&units=imperial")
-       .then(response => response.json())
-        .then(data => {
-        
-            for (var i=0; i<data.list.length; i++){
-                if (data.list[i].dt_txt.indexOf("12:00:00")!==-1) {
-                    var day = data.list[i];
-                    console.log(day);
+
+    //5 DAY FORECAST FUNCTION
+
+    function searchFiveDay(city) {
+        fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=974d902c878dcae370e669f524ad6ba0&units=imperial")
+            .then(response => response.json())
+            .then(data => {
+                
+
+                for (var i = 0; i < 5; i++) {
+                    // if (data.list[i].dt_txt.indexOf("12:00:00") !== -1) {
+                        var day = data.list[i];
+                        
+console.log(day);
+
+                        var dateFiveValue = day["sys"]["dt_text"];
+                        console.log(dateFiveValue);
+
+                        var weatherFiveValue = day["weather"]["0"];
+
+                        var tempFiveValue = day["main"]["temp"];
+
+                        var humFiveValue = day["main"]["humidity"];
+
+                        dateFivep.innerHTML = dateFiveValue;
+                        weatherFivep.innerHTML = weatherFiveValue;
+                        tempFivep.innerHTML = tempFiveValue;
+                        humFivep.innerHTML = humFiveValue;
+                    // }
                 }
-            }
 
 
-        });
-   }
+            });
+    }
+
+
+
 
 });
-
