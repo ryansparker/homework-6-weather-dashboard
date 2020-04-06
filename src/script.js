e9a0abc8d044b67aa8d5f8355d0631acbbd98ea8
Ryan Parker <ryansparker@gmail.com>
2020-04-05 21:51:01 -0400

    changed http to https

--- src/script.js
+++ src/script.js
@@ -76,7 +76,7 @@ $(document).ready(function () {
                 const x = data["coord"]["lat"];
                 const y = data["coord"]["lon"];
 
-                fetch("http://api.openweathermap.org/data/2.5/uvi?appid=974d902c878dcae370e669f524ad6ba0&lat=" + x + "&lon=" + y + "")
+                fetch("https://api.openweathermap.org/data/2.5/uvi?appid=974d902c878dcae370e669f524ad6ba0&lat=" + x + "&lon=" + y + "")
                     .then(response => response.json())
                     .then(data => {
                         const UVValue = data.value;
